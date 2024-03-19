fff2db41d31acfb0e483306d7d8b8981a566cf68
nick747 <nickodon747@proton.me>
2024-03-19 16:24:52 +0100

    feat: add calculator page

---
+++ lib/models/pages.dart
@@ -0,0 +1,8 @@
+import 'package:calcpack/screens/calculator.dart';
+import 'package:flutter/material.dart';
+import '../screens/percentage.dart';
+
+const Map<String, Widget> pages = {
+  "Calculator": MyCalculator(),
+  "Percentage": PercentageScreen(),
+};
